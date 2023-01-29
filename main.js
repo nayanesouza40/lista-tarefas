c2c14bb44f8bdf127c64788e1f63926281ec5d8f
Nayane Souza <113534680+nayanesouza40@users.noreply.github.com>
2023-01-29 20:49:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ $(document).ready(() => {
     $('#form').submit((e) => {
       e.preventDefault();
       var tarefa = $('#tarefa').val();      
-      $('#lista-tarefa').append('<li>' + tarefa + '</li>');
+      $('#lista-tarefa').append('<div> <li> <p>' + tarefa + '</p></li></div');
       $('#tarefa').val('');
     });
 
-    $('#lista-tarefa').on('click', 'li', function (){
+    $('#lista-tarefa').on('click', 'p', function (){
       $(this).toggleClass('checktarefa');
     });
+
+
 });
